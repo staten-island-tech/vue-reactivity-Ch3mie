ea2b86a316bbbac335e09fe6463abb2c5a8cf623
Ch3mie <ryanc668@nycstudents.net>
2025-02-05 12:18:18 -0500

    w

---
+++ Bananaclicker/src/components/ChooseCharacter.vue
@@ -0,0 +1,12 @@
+<template>
+  <form @submit="handleSubmit">
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script setup>
+const handleSubmit = (event) => {
+  event.preventDefault()
+  alert('Form submitted without refreshing!')
+}
+</script>
