594f3eb13b4fe2401c93d9641a626b56bebd7fcb
Ch3mie <ryanc668@nycstudents.net>
2025-02-11 12:06:50 -0500

    e

--- Bananaclicker/src/components/PrintCards.vue
+++ Bananaclicker/src/components/PrintCards.vue
@@ -6,7 +6,6 @@
         v-for="character in characters"
         :key="character.name"
         class="bg-gray-800 text-white p-6 rounded-2xl shadow-lg flex flex-col items-center transform transition duration-300 hover:scale-105 hover:shadow-2xl"
-        @click="animateCharacter($event)"
       >
         <div>
           <CharacterImage :character="character" />
@@ -31,6 +30,7 @@ import { ref } from 'vue'
 import CharacterImage from './CharacterImage.vue'
 import CharacterStats from './CharacterStats.vue'
 import ChooseCharacter from './ChooseCharacter.vue'
+import WeaponsCards from './WeaponsCards.vue'
 import { characters, selection } from '@/main'
 
 const container = ref(null) // Define container reference
