e42794d84f6e8c25c5e95353ec13c123a0fd30fd
Ch3mie <ryanc668@nycstudents.net>
2025-02-11 12:28:54 -0500

    e

--- Bananaclicker/src/components/PrintCards.vue
+++ Bananaclicker/src/components/PrintCards.vue
@@ -1,25 +1,27 @@
 <template>
-  <div ref="container" class="container mx-auto p-6">
-    <h1 class="text-3xl font-bold mb-6 text-center">Characters</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+  <div class="container mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-6 text-center">
+      {{ showWeapons ? 'Weapons' : 'Characters' }}
+    </h1>
+
+    <div v-if="!showWeapons" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <div
         v-for="character in characters"
         :key="character.name"
         class="bg-gray-800 text-white p-6 rounded-2xl shadow-lg flex flex-col items-center transform transition duration-300 hover:scale-105 hover:shadow-2xl"
       >
-        <div>
-          <CharacterImage :character="character" />
-        </div>
+        <CharacterImage :character="character" />
         <CharacterStats :character="character" />
-        <ChooseCharacter
-          @click="
-            () => {
-              selection(character)
-              clearHtml()
-            }
-          "
-          :character="character"
-        />
+        <ChooseCharacter @click="selectCharacter(character)" :character="character" />
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div
+        v-for="weapon in weapons"
+        :key="weapon.name"
+        class="bg-gray-800 text-white p-6 rounded-2xl shadow-lg flex flex-col items-center transform transition duration-300 hover:scale-105 hover:shadow-2xl"
+      >
+        <WeaponsCards :weapon="weapon" />
       </div>
     </div>
   </div>
@@ -31,13 +33,13 @@ import CharacterImage from './CharacterImage.vue'
 import CharacterStats from './CharacterStats.vue'
 import ChooseCharacter from './ChooseCharacter.vue'
 import WeaponsCards from './WeaponsCards.vue'
+import { weapons } from '@/main'
 import { characters, selection } from '@/main'
 
-const container = ref(null) // Define container reference
+const showWeapons = ref(false) // Track whether to show weapons
 
-const clearHtml = () => {
-  if (container.value) {
-    container.value.innerHTML = '' // Clears the container div content
-  }
+const selectCharacter = (character) => {
+  selection(character) // Call selection function
+  showWeapons.value = true // Hide characters and show weapons
 }
 </script>
